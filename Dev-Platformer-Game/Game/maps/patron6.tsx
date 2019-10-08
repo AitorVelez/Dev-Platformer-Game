17a86f706e025ec8a3829dc6c212f574da0e9c33
AitorVelez <aitorvelez@hotmail.com>
2019-10-08 20:53:40 +0200

    Level1 Map

---
+++ Dev-Platformer-Game/Game/maps/patron6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="patron6" tilewidth="32" tileheight="32" tilecount="3082" columns="67">
+ <image source="../../../Art/patron6.png" width="2156" height="1472"/>
+</tileset>
