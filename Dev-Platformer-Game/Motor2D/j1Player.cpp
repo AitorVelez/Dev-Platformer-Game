819e9861007247a2ab45b19c17deb3139198fb8c
MAtaur00 <mtmtarres@gmail.com>
2019-10-24 21:14:35 +0200

    load animation function done

--- Dev-Platformer-Game/Motor2D/j1Player.cpp
+++ Dev-Platformer-Game/Motor2D/j1Player.cpp
@@ -25,3 +25,14 @@ void j1Player::LoadTexture()
 	texture = App->tex->Load("textures/Player/player_spritesheet.png");
 }
 
+void j1Player::LoadAnimation(pugi::xml_node animation_node, Animation* animation)
+{
+	bool ret = true;
+
+	for (pugi::xml_node frame = animation_node.child("frame"); frame && ret; frame = frame.next_sibling("frame"))
+		animation->PushBack({ frame.attribute("x").as_int() , frame.attribute("y").as_int(), frame.attribute("w").as_int(), frame.attribute("h").as_int() });
+
+	animation->speed = animation_node.attribute("speed").as_float();
+	animation->loop = animation_node.attribute("loop").as_bool();
+	animation->offset = animation_node.attribute("offset").as_int();
+}
