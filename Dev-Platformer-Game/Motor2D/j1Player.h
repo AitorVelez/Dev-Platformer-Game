f854fb15bcef9f42652b046b58ea18478d0ca7dd
MAtaur00 <mtmtarres@gmail.com>
2019-10-24 11:16:52 +0200

    added animation and player module

---
+++ Dev-Platformer-Game/Motor2D/j1Player.h
@@ -0,0 +1,17 @@
+#ifndef _j1PLAYER_H__
+#define _j1PLAYER_H__
+
+#include "j1Module.h"
+#include "SDL/include/SDL.h"
+#include "p2Point.h"
+#include "Animation.h"
+
+class j1Player :
+	public j1Module
+{
+public:
+	j1Player();
+	~j1Player();
+};
+
+#endif
