6c217cb50b757276ff86eb8d5042dfb1aa25d2ba
AitorVelez <aitorvelez@hotmail.com>
2019-11-28 18:48:31 +0100

    Added brofiler files

--- Dev-Platformer-Game/Motor2D/j1Main.cpp
+++ Dev-Platformer-Game/Motor2D/j1Main.cpp
@@ -10,6 +10,10 @@
 #pragma comment( lib, "SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "SDL/libx86/SDL2main.lib" )
 
+#include "Brofiler/Brofiler.h"
+#pragma comment (lib,"Brofiler/ProfilerCore32.lib")
+
+
 enum MainState
 {
 	CREATE = 1,
@@ -78,8 +82,11 @@ int main(int argc, char* args[])
 
 			// Loop all modules until we are asked to leave ---------------------
 			case LOOP:
-			if(App->Update() == false)
-				state = CLEAN;
+			{
+				BROFILER_FRAME("frame");
+				if (App->Update() == false)
+					state = CLEAN;
+			}
 			break;
 
 			// Cleanup allocated memory -----------------------------------------
