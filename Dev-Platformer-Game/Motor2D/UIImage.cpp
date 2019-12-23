e4ec3b717ee51fd55755bfc5300aaa23022d0919
AlbertCayuela <albertcayuela16@gmail.com>
2019-12-23 20:28:18 +0100

    UIImage added (not working yet)

---
+++ Dev-Platformer-Game/Motor2D/UIImage.cpp
@@ -0,0 +1,16 @@
+#include "UIImage.h"
+#include "j1App.h"
+#include "j1Render.h"
+#include "Brofiler/Brofiler.h"
+
+UIImage::UIImage() : UIObject(x, y) {
+
+
+}
+UIImage::~UIImage() {}
+
+
+void UIImage::Draw() {
+
+
+}
---
+++ Dev-Platformer-Game/Motor2D/UIImage.h
@@ -0,0 +1,21 @@
+#ifndef _UI_IMAGE_
+#define _UI_IMAGE_
+
+#include "UIObject.h"
+#include "p2Point.h"
+
+class UIImage : public UIObject {
+
+
+
+
+public:
+
+	UIImage();
+	~UIImage();
+
+	void Draw();
+
+};
+
+#endif
